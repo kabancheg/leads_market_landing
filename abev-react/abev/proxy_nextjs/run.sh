995ae6c38266491d65c66a5de7cfd15f66d87a92
vlad <mozgalevvlad66@gmail.com>
2022-05-23 19:59:27 +0500

    Save state

--- abev-react/abev/proxy_nextjs/run.sh
+++ abev-react/abev/proxy_nextjs/run.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-cp /etc/nginx/default_old.conf.tpl /etc/nginx/conf.d/default.conf
+cp /etc/nginx/default.conf.tpl /etc/nginx/conf.d/default.conf
 
 while :; do sleep 6h & wait ${!}; nginx -s reload; done & nginx -g "daemon off;";
 
